d75745ec16d81e086ad758bcc433b2928da402ba
kirbyquerby <nathanxyzdias@gmail.com>
2016-08-06 13:37:05 -0500

    initial commit

---
+++ Worker/Worker/Java_Compiler/src/main/java/codex/main/Main.java
@@ -0,0 +1,13 @@
+package codex.main;
+
+/**
+ *
+ * @author Nathan Dias {@literal <nathanxyzdias@gmail.com>}
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        
+    }
+    
+}
