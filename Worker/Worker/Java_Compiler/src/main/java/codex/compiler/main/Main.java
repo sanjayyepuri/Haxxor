baad982348191e401e6132ea307b68707c28a03c
kirbyquerby <nathanxyzdias@gmail.com>
2016-08-06 16:11:55 -0500

    Runnable compiler completed

--- Worker/Worker/Java_Compiler/src/main/java/codex/compiler/main/Main.java
+++ Worker/Worker/Java_Compiler/src/main/java/codex/compiler/main/Main.java
@@ -1,16 +1,37 @@
 package codex.compiler.main;
 
+import codex.compiler.sandbox.SandboxSecurityManager;
+import codex.compiler.sandbox.SandboxTask;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /**
  *
  * @author Nathan Dias {@literal <nathanxyzdias@gmail.com>}
  */
 public class Main {
-    
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         String source = System.getenv("source");
         String input = System.getenv("input");
-        
+
+        SandboxTask task = new SandboxTask(source);
+
+        System.setIn(new BufferedInputStream(new ByteArrayInputStream(input.getBytes())));
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(output));
+
+        System.setSecurityManager(new SandboxSecurityManager());
+        task.run();
+
+        String out = output.toString();
+
+        System.getenv().put("output", out);
     }
-    
+
 }
---
+++ Worker/Worker/Java_Compiler/src/main/java/codex/compiler/sandbox/SandboxTask.java
@@ -0,0 +1,102 @@
+package codex.compiler.sandbox;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import javax.tools.ToolProvider;
+import org.mdkt.compiler.InMemoryJavaCompiler;
+
+/**
+ *
+ * @author Nathan Dias {@literal <nathanxyzdias@gmail.com>}
+ */
+public class SandboxTask {
+
+    private Method main;
+    boolean error = false;
+    private static boolean javapath = ToolProvider.getSystemJavaCompiler() != null;
+
+    final OutputStream out;
+
+    public SandboxTask(String src) {
+        super();
+        out = new ByteArrayOutputStream();
+        if (!javapath) {
+            throw new IllegalStateException("No java path set! Files will not compile!");
+        }
+
+        int bracecount = 0;
+        int dex = 0;
+
+        while (true) {
+            if (dex >= src.length() || !src.substring(dex).contains("class")) {
+                error = true;
+                return;
+            }
+            String curr = src.substring(dex);
+//            System.out.println(dex);
+            if (curr.startsWith("public") && curr.substring(0, curr.indexOf("class") + 6)
+                    .matches("public\\s+class\\s") && bracecount == 0) {
+
+                break;
+            }
+            if (src.charAt(dex) == '{') {
+                bracecount++;
+            }
+            if (src.charAt(dex) == '}') {
+                bracecount--;
+            }
+            dex++;
+
+        }
+
+        dex = src.substring(dex).indexOf("class") + 5 + dex;
+        while (Character.isWhitespace(src.charAt(dex))) {
+            dex++;
+        }
+
+        int dex2 = dex;
+        while (!Character.isWhitespace(src.charAt(dex2)) && !(src.charAt(dex2) == '{')) {
+            dex2++;
+        }
+
+        String classname = src.substring(dex, dex2);
+        System.out.println("Loaded class " + classname);
+
+        try {
+            Class clazz = InMemoryJavaCompiler.compile(classname, src);
+            main = clazz.getDeclaredMethod("main", String[].class);
+
+        } catch (Throwable t) {
+//            t.printStackTrace();
+            error = true;
+        }
+
+    }
+
+    public void run() {
+
+        if (error) {
+            System.out.println("error");
+            return;
+        }
+        try {
+            main.invoke(null, (Object) null);
+        } catch (Exception e) {
+            error = true;
+            e.printStackTrace(System.out);
+        }
+
+    }
+
+    public static void setJavaPath(String path) throws IllegalArgumentException {
+        System.setProperty("java.home", path);
+        if (javapath = (ToolProvider.getSystemJavaCompiler() != null)) {
+
+            System.out.println("java home successfully set");
+        } else {
+            throw new IllegalArgumentException("Invalid java home");
+        }
+    }
+
+}
