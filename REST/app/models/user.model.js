5784e39f6a558f30778779bed601c254e9ea456f
sanjayyepuri <sanjay.yepuri@gmail.com>
2016-08-06 12:42:51 -0500

    User Model password validation

---
+++ REST/app/models/user.model.js
@@ -0,0 +1,21 @@
+var mongoose  = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username: {type: String, unique: true, dropdups: true, required: true},
+    password: {type: String, required: true},
+    level: {type: Number, required: true},
+    competitor:{
+        type: Schema.ObjectId, 
+        ref: 'Competitor',
+        unique: true
+    } 
+})
+UserSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+UserSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+mongoose.model('User', UserSchema);
